ad3244b1a9c1ffb23adabfd66600936315d0327f
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2023-04-04 11:13:34 +0300

    Draft 2
    1. data source bean for PostgreSQL configured and injected;
    2. Hikari connection pool configured.

--- src/main/java/com/streamlined/library/LibraryApplication.java
+++ src/main/java/com/streamlined/library/LibraryApplication.java
@@ -3,20 +3,25 @@ package com.streamlined.library;
 import java.util.Arrays;
 import java.util.Date;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@Log4j2
+@Slf4j
 public class LibraryApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private ApplicationContext context;
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
@@ -28,6 +33,20 @@ public class LibraryApplication implements CommandLineRunner {
 		log.info("Total number of bean definitions {}", context.getBeanDefinitionCount());
 		log.info("Logger implementation class {}", log.getClass().getName());
 		log.info("Application startup date/time {}", new Date(context.getStartupDate()));
+		log.info("DataSource class name {}", dataSource.getClass().getName());
+		try (var connection = dataSource.getConnection()) {
+			final var metaData = connection.getMetaData();
+			log.info("Database product name {}, version {} ({}.{})", metaData.getDatabaseProductName(),
+					metaData.getDatabaseProductVersion(), metaData.getDatabaseMajorVersion(),
+					metaData.getDatabaseMinorVersion());
+			log.info("Data source driver name {} and version {}.{}", metaData.getDriverName(),
+					metaData.getDriverMajorVersion(), metaData.getDriverMinorVersion());
+			log.info("Datasource URL {}, user name '{}' (max length {})", metaData.getURL(), metaData.getUserName(),
+					metaData.getMaxUserNameLength());
+			for (var schemas = metaData.getSchemas(); schemas.next();) {
+				log.info("schema name {}", schemas.getObject(1));
+			}
+		}
 	}
 
 }
