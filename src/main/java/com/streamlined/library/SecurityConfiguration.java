315b6e4e77ccf79d94368d44a28414ee993989a2
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2023-05-15 14:49:26 +0300

    Draft 32. In-memory user authentication replaced with JDBC-based
    1. Added UserDetailsService bean with custom user/authority fetch
    queries to SecurityConfiguration class;
    2. Added 'same origin' extra restriction to SecurityFilterChain bean;
    3. PasswordEncoder bean implemented as DelegatingPasswordEncoder;
    4. Added authenticated user role info to start page 'index.html'.

--- src/main/java/com/streamlined/library/SecurityConfiguration.java
+++ src/main/java/com/streamlined/library/SecurityConfiguration.java
@@ -1,25 +1,59 @@
 package com.streamlined.library;
 
+import java.util.HashMap;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
+	private static final String USERS_BY_USERNAME_QUERY = """
+			select u.login, u.password_hash, 1 from (
+			 select c.login, c.password_hash from customer c
+			 union all 
+			 select l.login, l.password_hash from librarian l
+			 union all 
+			 select m.login, m.password_hash from manager m
+			) as u
+			where u.login = ?
+			""";
+	private static final String AUTHORITIES_BY_USERNAME_QUERY = """
+			select u.login, u.role from (
+			 select c.login, 'ROLE_CUSTOMER' as role from customer c
+			 union all 
+			 select l.login, 'ROLE_LIBRARIAN' as role from librarian l
+			 union all 
+			 select m.login, 'ROLE_MANAGER' as role from manager m
+			) as u
+			where u.login = ?
+			""";
+
+	private final DataSource dataSource;
+
 	@Bean
-	public UserDetailsManager userDetailsManager() {
-		UserDetails user = User.withUsername("simple_user").password("simple_password")
-				.passwordEncoder(passwordEncoder()::encode).roles("USER").build();
-		UserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(user);
+	public UserDetailsService userDetailsService() {
+		var manager = new JdbcUserDetailsManager(dataSource);
+		manager.setUsersByUsernameQuery(USERS_BY_USERNAME_QUERY);
+		manager.setAuthoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME_QUERY);
 		return manager;
 	}
 
@@ -31,13 +65,20 @@ public class SecurityConfiguration {
 		httpSecurity.formLogin(formLogin -> formLogin.loginPage("/user/checkin").loginProcessingUrl("/user/do-login")
 				.usernameParameter("login").passwordParameter("password").defaultSuccessUrl("/")
 				.failureUrl("/user/checkin?error=true"));
-		httpSecurity.logout().logoutUrl("/user/checkout").logoutSuccessUrl("/");
+		httpSecurity.logout().logoutUrl("/user/checkout").logoutSuccessUrl("/").invalidateHttpSession(true)
+				.deleteCookies("JSESSIONID");
+		httpSecurity.headers().frameOptions().sameOrigin();
 		return httpSecurity.build();
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		var encoders = new HashMap<String, PasswordEncoder>();
+		encoders.put("bcrypt", new BCryptPasswordEncoder(BCryptVersion.$2Y));
+		encoders.put("scrypt", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+		encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+		encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+		return new DelegatingPasswordEncoder("bcrypt", encoders);
 	}
 
 }
